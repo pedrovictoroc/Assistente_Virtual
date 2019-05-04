49a91daf85ac5760a30ee330b8b21c171edfde30
Pedro Victor <37453874+stormsamurai1@users.noreply.github.com>
2019-05-04 13:41:11 -0300

    Add files via upload

--- Stark Project/main.py
+++ Stark Project/main.py
@@ -1,46 +1,89 @@
 #-*- coding: utf-8 -*-
 
-#importando os módulos do chatbot
+#import section
 
-from chatterbot.trainers import ListTrainer
-from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer #treinador do BOT
+from chatterbot import ChatBot #Base para utilizar o BOT
+import os   #Importa as funções do sistema operacional
+import speech_recognition as sr #Base para o reconhecimento de voz
+import pyttsx3  #Usado para devolução de resposta com audio pelo BOT
+import time #Usado para funções de tempo
 
-import os
+def saudar_ao_iniciar(hora,speaker):    #Iniciador com suadações Pyttsx3 e Time
+    if(0 <= hora <=5):
+        resposta = "Esta tarde, você deveria dormir!"
+        speak(resposta,speaker)
+    elif(6<= hora <=12):
+        resposta = "Bom dia, como posso ajudar?"
+        speak(resposta,speaker)   
+    elif(13 <= hora <= 17):
+        resposta = "Boa tarde, como posso ajudar?"
+        speak(resposta,speaker)       
+    elif(18 <= hora <= 23):
+        resposta = "Boa noite, como posso ajudar?"
+        speak(resposta,speaker)
 
-import speech_recognition as sr
-import pyttsx3  #para usar o speak
+def dizer_hora(speaker):    #Função de devolução de hora usando Pyttsx3 e Time 
+    hora = time.localtime()[3]
+    minuto = time.localtime()[4]
+    segundo = time.localtime()[5]
 
-speaker = pyttsx3.init()
+    resposta = ("São " +str(hora)+ " horas e " +str(minuto)+ " minutos")
+    speak(resposta,speaker)
+    
 
-bot = ChatBot('Stark',read_only = True)
+def procedimento_de_fala(bot,speaker):  #Procedimento de fala que usa o banco de dados do BOT
+    recognizer = sr.Recognizer() #Capta o reconhecedo de voz
 
-"""
-trainer = ListTrainer(bot) #definir o treinamento
+    with sr.Microphone() as sound_user: #Captura a voz do usuario
+        recognizer.adjust_for_ambient_noise(sound_user) #Ajusta para ignorar ruidos
+        continuar = True
+        
+        while continuar:
+            try:
+                audio = recognizer.listen(sound_user)
+                speech = recognizer.recognize_google(audio, language = 'pt')    #Usando a base google em português
+                print("Você: ", speech) #Saída de terminal para verificação de entrada
+                response = bot.get_response(speech) #Resposta compatível com o que foi dito
+                #print("Bot: ", response)    #saída para terminal para verificação de saída
+                #print("") #Quebra de linha
+                
+                if(speech == "Que horas são" or speech == "diga a hora"):
+                    dizer_hora(speaker)
+                if(speech == "finalizar" or "parar"): #Para a execução
+                    continuar = False
+                else:
+                    speak(response,speaker) #Pronunciar a resposta do BOT
+            except:
+                speak("Algo errado ocorreu",speaker)    #Aviso de erro com som
+    
 
-for _file in os.listdir('chats'): #percorrer todos os arquivos em chats
-    lines = open('chats/' + _file, 'r').readlines() #ler linhas
+def treinar_bot(bot):
+    trainer = ListTrainer(bot) #Seta um treinador para o BOT
+    for _file in os.listdir('chats'):
+        
+        #Para cada arquivo na pasta chats ler todas as linhas e captar respostas e perguntas
 
-    trainer.train(lines)
-"""
-def speak(text):
+        lines = open('chats/' + _file, 'r').readlines() 
+        trainer.train(lines)
+
+def speak(text,speaker):    #Função para que o BOT responda com audio
     speaker.say(text)
     speaker.runAndWait()
 
-r = sr.Recognizer() #reconhecer a voz
+def main():
+    speaker = pyttsx3.init()    #Inicio o speaker
 
-with sr.Microphone() as s:  #captar a voz na variavle s
-    r.adjust_for_ambient_noise(s) #configurar o ruido
+    bot = ChatBot('Jarvis',read_only = True)    #Defino o nome do meu bot e afirmo que somente lerá arquivos
+    deve_treinar = input("Deseja treinar o BOT?: (S) (N)")  #Pergunta ao usuario se ocorreu alterações no Banco de Dados
 
-    while True:
-        try:
-            audio = r.listen(s)
+    saudar_ao_iniciar(time.localtime()[3],speaker)  #Real Inicio do Programa
 
-            speech = r.recognize_google(audio,language = 'pt') #usando o googlee e a lingua portuguesa
-            
-            print("Você disse: ", speech)
-            response = bot.get_response(speech)
-            print("Bot:", response)
-            print("")
-            speak(response)
-        except:
-                speak('Algum erro ocorreu')   
+    if(deve_treinar.upper() == "S"):    #Inicia o programa treinando o BOT
+        treinar_bot(bot)
+        procedimento_de_fala(bot,speaker)
+    else:
+        procedimento_de_fala(bot,speaker)
+
+
+main() #Inicia o programa
